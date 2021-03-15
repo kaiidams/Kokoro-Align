b72c5a675c41a7512aac43574c1b59dee8549bed
Katsuya Iida <katsuya.iida@gmail.com>
2021-03-15 23:14:38 +0900

    update

--- voice100/trans.py
+++ voice100/trans.py
@@ -1,26 +1,9 @@
 from voice100._text2voca import text2voca
 from bs4 import BeautifulSoup
-#from lxml import etree
-
-class a():
-    def start(self, tag, attrib):
-        print("start %s %r" % (tag, dict(attrib)))
-    def end(self, tag):
-        print("end %s" % tag)
-    def data(self, data):
-        print("data %r" % data)
-    def comment(self, text):
-        print("comment %s" % text)
-    def close(self):
-        print("close")
-        return "closed!"    
 
 with open('a.html', 'rt', encoding='shift_jis') as f:
     soup = BeautifulSoup(f, 'html.parser')
-    #parser = etree.HTMLParser(target = a())
-    #tree = etree.parse(f, parser)
 
-#print(tree)
 body = soup.find('body')
 
 s = ''
@@ -35,7 +18,6 @@ def f(node):
         if node.name == 'ruby':
             rb = ''.join(child.string for child in node.find_all('rb') if child.string)
             rt = ''.join(child.string for child in node.find_all('rt') if child.string)
-            #print(rb, rt)
             s += rb
         elif node.name == 'br':
             g(s)
@@ -49,12 +31,3 @@ def f(node):
     else:
         s += node
 f(body)
-
-if False:
-    for node in soup.descendants:
-        if node.name:
-            if node.name == 'rp':
-                for x in node.strings:
-                    pass #print(x)
-        else:
-            pass #print(node)
