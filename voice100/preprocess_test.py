b6aef91b17a1dc231109a5826114e22545b8546f
Katsuya Iida <katsuya.iida@gmail.com>
2021-03-16 14:49:28 +0900

    update

--- voice100/preprocess_test.py
+++ voice100/preprocess_test.py
@@ -5,12 +5,12 @@ from tqdm import tqdm
 from .vocoder import writewav, decode_audio
 from .encoder import decode_text
 
-def test_data(name='css10ja'):
-    file = 'data/%s_text.npz' % name
+def test_text_data():
+    file = 'data/%s_text.npz' % args.dataset
     with np.load(file) as f:
         text_index = f['index']
         text_data = f['data']
-    file = 'data/%s_audio_16000.npz' % name
+    file = 'data/%s_audio_16000.npz' % args.dataset
     with np.load(file) as f:
         audio_index = f['index']
         audio_data = f['data']
@@ -24,12 +24,33 @@ def test_data(name='css10ja'):
         audio = audio_data[audio_start:audio_end, :]
         print(decode_text(text))
         x = decode_audio(audio)
-        file = 'data/synthesized/%s_%04d.wav' % (name, index)
+        file = 'data/synthesized/%s_%04d.wav' % (dataset, index)
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        writewav(file, x)
+
+def test_audio_data(args):
+    file = 'data/%s_audio_16000.npz' % args.dataset
+    with np.load(file) as f:
+        audio_index = f['index']
+        audio_data = f['data']
+
+    for index in tqdm(range(10)):
+        audio_start = audio_index[index - 1] if index else 0
+        audio_end = audio_index[index]
+        audio = audio_data[audio_start:audio_end, :]
+        x = decode_audio(audio)
+        file = 'data/synthesized/%s_%04d.wav' % (dataset, index)
         os.makedirs(os.path.dirname(file), exist_ok=True)
         writewav(file, x)
 
 def main():
-    test_data()#'tsukuyomi_normal')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--analyze', action='store_true', help='Analyze F0 of sampled data.')
+    parser.add_argument('--normalize', action='store_true', help='Compute normalization parameters.')
+    parser.add_argument('--dataset', help='Dataset to process, css10ja, tsukuyomi_normal')
+
+    args = parser.parse_args()
+    test_audio_data(args)
 
 if __name__ == '__main__':
     main()
