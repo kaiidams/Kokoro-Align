9ae3da46e98dbd235df394b086e8057844dfaf04
Katsuya Iida <katsuya.iida@gmail.com>
2022-11-29 19:31:44 +0900

    Fix getyomi()

--- kokoro_align/_text2voca.py
+++ kokoro_align/_text2voca.py
@@ -3,6 +3,7 @@
 import fugashi
 from ._yomi2voca import yomi2voca
 from typing import List, Tuple
+import re
 
 _tagger = fugashi.Tagger()
 
@@ -10,19 +11,20 @@ _katakana = ''.join(chr(ch) for ch in range(ord('ァ'), ord('ン') + 1))
 _hiragana = ''.join(chr(ch) for ch in range(ord('ぁ'), ord('ん') + 1))
 _kata2hiratrans = str.maketrans(_katakana, _hiragana)
 _symbols_tokens = set(['・', '、', '。', '？', '！'])
-_no_yomi_tokens = set(['「', '」', '『', '』', '―', '（', '）', '［', '］', '[', ']', '　', '…'])
+_no_yomi_tokens = set(['「', '」', '『', '』', '―', '（', '）', '［', '］', '[', ']', '　', '‥', '…'])
+_no_yomi_rx = re.compile(r'[][「」『』―（）［］　‥…]')
 
 
-def kata2hira(text):
+def kata2hira(text: str) -> str:
     text = text.translate(_kata2hiratrans)
     return text.replace('ヴ', 'う゛')
 
 
 def getyomi(text: str) -> List[Tuple[str, str]]:
-    return getyomi_unidic(text)
+    return getyomi_fugashi(text)
 
 
-def getyomi_unidic(text: str) -> List[Tuple[str, str]]:
+def getyomi_fugashi(text: str) -> List[Tuple[str, str]]:
     parsed: str = _tagger.parse(text)
     res = []
     for line in parsed.split('\n'):
@@ -33,24 +35,22 @@ def getyomi_unidic(text: str) -> List[Tuple[str, str]]:
 
         yomi = parts[9] if len(parts) >= 10 else ''
         if yomi:
-            if yomi in ['Ｋ']:
-                yomi = 'けい'
-            else:
-                yomi = kata2hira(yomi)
+            yomi = kata2hira(yomi)
             res.append((word, yomi))
         else:
-            if word in _symbols_tokens:
-                res.append((word, word))
-            elif word == 'っ' or word == 'ッ':
+            yomi = _no_yomi_rx.sub('', word)
+            if yomi in ['Ｋ']:
+                res.append((word, 'けい'))
+            elif yomi in _symbols_tokens:
+                res.append((word, yomi))
+            elif yomi == 'っ' or yomi == 'ッ':
                 res.append((word, 'っ'))
-            elif word in _no_yomi_tokens:
-                res.append((word, ''))
             else:
-                res.append((word, word))
+                yomi = kata2hira(yomi)
     return res
 
 
-def getyomi_unidic_lite(text: str) -> List[Tuple[str, str]]:
+def getyomi_mecab(text: str) -> List[Tuple[str, str]]:
     parsed: str = _tagger.parse(text)
     res = []
     for line in parsed.split('\n'):
